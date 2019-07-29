d993f9dd0ddcaec4d6307d699cb150d49d26889c
chitai965123@gmail.com <0983157421bB>
2019-07-29 17:02:58 +0700

    Load TKB

--- src/IO_File/FileManager.java
+++ src/IO_File/FileManager.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 
 import Model.ModelManager;
 import Model.SinhVien;
+import Model.ThoiKhoaBieu;
 import Model.TaiKhoan;
 
 public class FileManager {
@@ -76,6 +77,41 @@ public class FileManager {
 			e.printStackTrace();
 		}finally {
 			
+		}
+		return false;
+	}
+	
+	
+	public static boolean DocFileThoiKhoaBieu(String path) {
+		if(ModelManager.TKB.size()>0) {
+			ModelManager.TKB=new ArrayList<>();
+		}
+		int count=0;
+		try {
+			FileInputStream fileInputStream=new FileInputStream(path);
+			InputStreamReader inputStreamReader=new InputStreamReader(fileInputStream,"UTF-8");
+			BufferedReader bufferedReader=new BufferedReader(inputStreamReader);
+			String line=bufferedReader.readLine();
+			while(line!=null) {
+				count ++;
+				if(count>=2) {
+					String []arr=line.split(",");
+					if(arr.length>0) {
+	                    int stt=Integer.parseInt(arr[0]);
+						ThoiKhoaBieu tkb = new ThoiKhoaBieu(stt, arr[1], arr[2], arr[3]);
+						ModelManager.TKB.add(tkb);
+					}
+				}
+				
+				line=bufferedReader.readLine();
+			}
+			return true;
+		} catch (FileNotFoundException | UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		return false;
 	}
--- src/Model/ModelManager.java
+++ src/Model/ModelManager.java
@@ -5,12 +5,21 @@ import java.util.ArrayList;
 public class ModelManager {
 	public static ArrayList<SinhVien>DSSV=new ArrayList<>();
 	public static ArrayList<TaiKhoan>DSTK=new ArrayList<>();
+	public static ArrayList<ThoiKhoaBieu>TKB=new ArrayList<>();
 	public static ArrayList<SinhVien> getDSSV() {
 		return DSSV;
 	}
 
 	public static void setDSSV(ArrayList<SinhVien> dSSV) {
 		DSSV = dSSV;
+	}
+	
+	public static ArrayList<ThoiKhoaBieu> getTKB() {
+		return TKB;
+	}
+
+	public static void setTKB(ArrayList<ThoiKhoaBieu> dTKB) {
+		TKB = dTKB;
 	}
 	
 
